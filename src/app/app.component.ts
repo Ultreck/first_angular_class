1df5663949a5a03bf18ad43d3ffbf54b6c1f7e00
Ultreck <adetutuemmanueloluwatayese@gmail.com>
2023-03-22 14:37:36 +0100

    first angular class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LocalService} from './local.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private localStore: LocalService) { }
+
   title = 'angularclass';
+  public name = "John Deo";
+  public firstname:String = "";
+  public lastname:String = "";
+  public age:String = "";
+  public password:String = "";
+  public email:String = "";
+  public data:Object = {};
+  public student:Array< Object> = [];
+  public localData:any;
+  public userData = "";
+
+  // private num = 1;
+  // protected num2 = 2;
+  // publick is known as access modifier;
+  public num:number = 5;
+  // public student:Array< Object> = [
+  //   {name:"Janet", dept:"Siftware", age:12}
+  // ];
+  showName = () =>{
+    alert(this.name)
+  }
+  getData = () =>{
+    console.log(this
+      .userData);
+    
+  }
+  addUsers = () =>{
+    this.data = {firstname:this.firstname, lastname:this.lastname, age:this.age,  password:this.password,  email:this.email};
+    this.student.push(this.data);
+    this.localStore.saveData('anData', this.student);
+  }
+
+  
+  getUsers = () =>{
+    this.localData =  this.localStore.getData("ngData");
+    console.log( this.localData);
+  }
+
+  // onPageLoad = () =>{
+  //   let dat =localStorage.getItem("ngData")
+  //   console.log(dat);
+  //   alert(dat)
+  // }
+
 }
---
+++ src/app/local.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalService {
+
+  constructor() { }
+  public saveData(key: string, value: any) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  public getData(key: string): any {
+    const value = localStorage.getItem(key);
+    return value? JSON.parse(value): null;    
+  }
+  public removeData(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  public clearData() {
+    localStorage.clear();
+  }
+}
